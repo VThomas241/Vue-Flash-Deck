a749ac5eddaf78996f70584591fd162ec9dcf599
VThomas241 <vivekthomasnitro1@gmail.com>
2023-08-20 16:12:32 +0530

    Started on Dashboard view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
+    async function populateDecks(token:String) {
+        const res = await fetch('http://127.0.0.1:5000/decks/',{
+            method: 'GET',
+            mode:'cors',
+        }).then(data=>data.json()).catch(e=>console.log(e))
 
+        return res
+    }
+    
+    
+    let token = localStorage.getItem('token')
+    if (token){
+        let decks = populateDecks(token).then(data=>data).catch(e=>{console.log(e)})
+
+        console.log(decks)
+    }
 </script>
 
 <template>
